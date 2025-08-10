7a159e9909aa4496a546894c0f74fb8662b9b4d0
Jinwoo Woo <114578011+Jinunu99@users.noreply.github.com>
2025-08-10 14:28:12 +0900

    Delete

--- adas_control/Core/Inc/handle.h
+++
@@ -1,9 +0,0 @@
-#ifndef INC_HANDLE_H_
-#define INC_HANDLE_H_
-
-#include "tim.h"
-
-void handle_INIT();
-void handle_CONTROL(uint16_t val);
-
-#endif /* INC_HANDLE_H_ */
--- adas_control/Core/Inc/mycan.h
+++
@@ -1,9 +0,0 @@
-#ifndef INC_MYCAN_H_
-#define INC_MYCAN_H_
-
-#include "main.h"
-#include "can.h"
-
-void filter_INIT(uint32_t f_maskid, uint32_t f_id);
-
-#endif /* INC_MYCAN_H_ */
